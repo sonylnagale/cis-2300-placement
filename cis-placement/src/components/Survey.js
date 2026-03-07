11b6232385c3b581f095522f664014f128d6d0d9
Sonyl Nagale <sonylnagale@users.noreply.github.com>
2026-03-07 18:37:12 -0500

    React with re-render of question CSS

--- cis-placement/src/components/Survey.js
+++ cis-placement/src/components/Survey.js
@@ -3,9 +3,8 @@
 import "@/app/globals.css";
 
 import { useState, useEffect } from 'react';
-import { Model } from 'survey-core';
+import { Model, Serializer, settings, QuestionHtmlModel } from 'survey-core';
 import { Survey } from 'survey-react-ui';
-import { settings } from 'survey-core';
 
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github-dark.css';
@@ -19,7 +18,6 @@ import "@/components/survey.css";
 
 hljs.registerLanguage('python', python);
 
-
 export default function SurveyComponent() {
   const [survey] = useState(new Model(model));
 
@@ -30,19 +28,29 @@ export default function SurveyComponent() {
 
     survey.onTextMarkdown.add((_, options) => {
       options.html = converter.renderInline(options.text);
-      hljs.highlightAll();
+
     });
 
     survey.onUpdateQuestionCssClasses.add((_, options) => {
-      console.log(options.question.getPlainData().name)
       if (options.question.getType() === "html" && (options.question.getPlainData().name !== "Introduction" && options.question.getPlainData().name !== "Background" && options.question.getPlainData().title !== "2300")) {
         options.cssClasses.root = "question-root";
-      }    
+      }
     });
 
 
     survey.onCurrentPageChanged.add((_, options) => {
       window.location.hash = options.newCurrentPage.jsonObj.name;
+      survey.currentPage.elementsValue.forEach(async (el) => {
+        // make sure the code questions get a fresh highlight when visible
+        if (el instanceof QuestionHtmlModel) {
+          try {
+            const value = await waitForProperty(el, 'react');
+            hljs.highlightElement(value.rootRef.current.querySelector("pre code"));
+          } catch (error) {
+            // silent
+          }
+        }
+      });
     });
 
     settings.triggers.executeSkipOnValueChanged = false;
@@ -66,6 +74,23 @@ export default function SurveyComponent() {
 
   }, []);
 
+  function waitForProperty(obj, propName, timeout = 3000) {
+    return new Promise((resolve, reject) => {
+      const startTime = Date.now();
+
+      const check = () => {
+        if (propName in obj) {
+          resolve(obj[propName]);
+        } else if (Date.now() - startTime >= timeout) {
+          reject(new Error(`Timeout waiting for property "${propName}"`));
+        } else {
+          setTimeout(check, 10);
+        }
+      };
+
+      check();
+    });
+  }
 
   function saveData(survey) {
     window.localStorage.setItem(
@@ -78,7 +103,9 @@ export default function SurveyComponent() {
     );
   }
 
-
+  Serializer.addProperty("itemvalue", {
+    name: "score:number"
+  });
 
   const STORAGE_ITEM_DATA_KEY = "baruch-cis-self-placement";
   const STORAGE_ITEM_UI_STATE_KEY = "baruch-cis-self-placement-ui";
