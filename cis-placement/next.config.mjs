741b30ca2fe4667e2f5b7a2fb6b288634256cd5f
Sonyl Nagale <sonylnagale@users.noreply.github.com>
2026-03-09 01:07:53 -0400

    DistDir

--- cis-placement/next.config.mjs
+++ cis-placement/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "out",
-  output: "export"
+  output: "export",
+  basePath: "/cis-placement",
 };
 
 export default nextConfig;
